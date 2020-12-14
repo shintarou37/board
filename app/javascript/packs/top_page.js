0f475ca750b3288d49751a08d8246ce4a071ebd6
shintarou37 <m.sintarou37@gmail.com>
2020-12-14 19:04:44 +0900

    非同期機能大枠

--- app/javascript/packs/top_page.js
+++ app/javascript/packs/top_page.js
@@ -1,3 +1,36 @@
-$(function(){
+function load() {
   
-});
+  const submit_post = document.getElementById("submit_post")
+  submit_post.addEventListener("click",(e)=>{
+    console.log('aaaaa')
+    // debugger
+    const formData = new FormData(document.getElementById("form"));
+    
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/collects", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const collect = XHR.response.collect;
+      const list = document.getElementById("list");
+      const formText_1 = document.getElementById("content-1" );
+      const formText_2 = document.getElementById("content-2");
+      const HTML = `
+      <div class="post-content">
+          投稿日時：${collect.explanation}
+      </div>
+      <div class="post-content">
+          投稿日時：${collect.language_id}
+      </div>`;
+      list.insertAdjacentHTML("beforebegin", HTML);
+      formText_1.value = "";
+      formText_2.value = "";
+    };
+    e.preventDefault();
+  })
+}
+window.addEventListener("load", load);
