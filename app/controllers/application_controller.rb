c6d5310ab634233ec9cdc544fd1886c141c2c7fb
shintarou37 <m.sintarou37@gmail.com>
2020-12-14 11:02:34 +0900

    ActiveStrage導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
   
   def configre_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduce, :age_id, :prefectures_id, :language_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduce, :age_id, :prefectures_id, :language_id,:image])
   end
 
 end
