554ce991eafbe261491a39797ff60c38e0786669
shintarou37 <m.sintarou37@gmail.com>
2020-12-15 12:24:50 +0900

    userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  with_options presence: true do
+    validates :nickname
+    validates :prefectures_id
+  end
 end
