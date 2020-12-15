17d29504edea5af0fd7a9085b94319c04637d5e0
shintarou37 <m.sintarou37@gmail.com>
2020-12-15 12:49:35 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+  belongs_to :prefecture
+  belongs_to :age
+  belongs_to :language
+  belongs_to :type
+  belongs_to :goal
   with_options presence: true do
     validates :nickname
     validates :prefectures_id
+  end
+  with_options numericality: { other_than: 1 }
+    validates :prefectures_id
+    validates :age_id
+    validates :language_id
+    validates :type_id
+    validates :goal_id
   end
 end
