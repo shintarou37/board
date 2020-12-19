0f681b41443a2dbccf14161704fa76e5ba2d8664
shintarou37 <m.sintarou37@gmail.com>
2020-12-19 11:48:24 +0900

    ビュー整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
-    
     @user_show = User.find(params[:id])
+  end
+
+  def edit 
+    @user_show = User.find(params[:id])
+  end
+
+  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show,:edit,:update]
   root to: 'collects#index'
   resources :collects do
     resources :comments, only: :create
