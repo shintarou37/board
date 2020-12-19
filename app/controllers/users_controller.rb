f4eaae1aea29cbc2d0b651664bcaceee3c3a005f
shintarou37 <m.sintarou37@gmail.com>
2020-12-19 12:02:18 +0900

    userコントローラー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
+  before_action :user_show, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit, :update, :destory]
   def show
-    @user_show = User.find(params[:id])
   end
 
   def edit 
-    @user_show = User.find(params[:id])
   end
 
   def update
+  end
+
+  private 
+
+  def user_show
+    @user_show = User.find(params[:id])
+  end
+  def move_to_index
+    unless user_signed_in? || current_user.id == @user_show.id
+    redirect_to root_path
+    end
   end
 end
