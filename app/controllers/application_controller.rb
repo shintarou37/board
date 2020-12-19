b4b436e955a1dba94b03687d928ef7e178244657
shintarou37 <m.sintarou37@gmail.com>
2020-12-19 13:01:18 +0900

    ユーザー編集機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_params, only: [:update], if: :devise_controller?
   
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduce,:image,:GitHub,:age_id,:prefecture_id,:language_id,:type_id,:goal_id])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :introduce,:image,:GitHub,:age_id,:prefecture_id,:language_id,:type_id,:goal_id])
   end
 
 end
