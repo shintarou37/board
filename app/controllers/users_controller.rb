4e18268258ec140898b573c273b5e340f90d3fa4
shintarou37 <m.sintarou37@gmail.com>
2020-12-19 12:42:57 +0900

    編集場所変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
-  before_action :user_show, only: [:edit, :show]
-  before_action :move_to_index, only: [:edit, :update, :destory]
   def show
-  end
-
-  def edit 
-  end
-
-  def update
-  end
-
-  private 
-
-  def user_show
     @user_show = User.find(params[:id])
-  end
-  def move_to_index
-    unless user_signed_in? || current_user.id == @user_show.id
-    redirect_to root_path
-    end
   end
 end
