c53e2343ec3bbf0dfdd49149c4127aa104e8c585
shintarou37 <m.sintarou37@gmail.com>
2020-12-28 17:47:37 +0900

    hidden削除

--- app/javascript/packs/hidden.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  var url = location.href ;
-  if (url === "http://3.131.108.243/collects/new"){
-    $('.test').on('click', function() {
-      $('.search_hidden').show()
-    });
- };
-});
