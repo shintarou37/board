40a94ba8ab1d8088b78264cec0068619e265b759
shintarou37 <m.sintarou37@gmail.com>
2020-12-28 17:39:55 +0900

    本番環境設定

--- app/javascript/packs/animation.js
+++ app/javascript/packs/animation.js
@@ -1,7 +1,6 @@
 $(function(){
   var url = location.href ;
-  if (url === "http://localhost:3000/"){
-    // "http://3.131.108.243/"("本番環境")
+  if (url === "http://3.131.108.243/"){
     $(".typed").fadeIn(2700);
   };
 });
--- app/javascript/packs/hidden.js
+++ app/javascript/packs/hidden.js
@@ -1,7 +1,6 @@
 $(function(){
   var url = location.href ;
-  if (url === "http://localhost:3000/collects/new"){
-    // http://3.131.108.243/collects/new(本番環境)
+  if (url === "http://3.131.108.243/collects/new"){
     $('.test').on('click', function() {
       $('.search_hidden').show()
     });
